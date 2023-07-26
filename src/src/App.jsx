b274e8362bf71de370f6bd41dbbc6d97edbc02aa
Willian Sato <willianmassayukisato@gmail.com>
2023-07-26 09:48:22 -0300

    Preparação inicial

---
+++ src/src/App.jsx
@@ -0,0 +1,18 @@
+import './styles/app.scss'
+import Header from './components/Header'
+import Generico from './components/Generico'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <h1>Começo do app</h1>
+        <Header />
+        <Generico />
+      </div>       
+    </>
+  )
+}
+
+export default App
---
+++ src/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import '../styles/components/header.scss';
+
+const Header = () => {
+  return (
+    <div className="header"></div>
+  )
+}
+
+export default Header
